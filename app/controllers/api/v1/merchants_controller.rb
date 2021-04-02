0154306f0c03d2e191e044514be5fc65e7a27de3
abreaux26 <angelbreaux26@gmail.com>
2021-04-02 13:47:13 -0500

    Create specs for all merchants

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,25 @@
 class Api::V1::MerchantsController < ApplicationController
+  # before_action :set_page, :set_per_page, only: [:index]
+
   def index
-    merchants = Merchant.first(20)
+    @page = params.fetch(:page,1).to_i
+    @per_page = params.fetch(:per_page,20).to_i
+
+    merchants = Merchant.limit(@per_page)
+
+    if @page > 1
+      merchants = merchants.offset((@page-1) * 20)
+    end
+
     render json: MerchantSerializer.format_merchants(merchants)
   end
+
+  # private
+  # def set_page
+  #   @page = params.fetch(:page,1).to_i
+  # end
+  #
+  # def set_per_page
+  #   @per_page = params.fetch(:per_page,20).to_i
+  # end
 end
--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -1,26 +1,102 @@
 require 'rails_helper'
 
 RSpec.describe "Merchants API" do
-  it "sends a list of merchants" do
-    create_list(:merchant, 3)
+  before :each do
+    create_list(:merchant, 100)
+  end
 
-    get '/api/v1/merchants'
+  describe 'happy path' do
+    it "sends a list of merchants" do
+      get '/api/v1/merchants'
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+      merchants = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants[:data].count).to eq(3)
-    
-    merchants[:data].each do |merchant|
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id].to_i).to be_an(Integer)
+      expect(merchants[:data].count).to eq(20)
 
-      expect(merchant).to have_key(:type)
-      expect(merchant[:type]).to eq('merchant')
+      merchants[:data].each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id].to_i).to be_an(Integer)
 
-      expect(merchant[:attributes]).to have_key(:name)
-      expect(merchant[:attributes][:name]).to be_a(String)
+        expect(merchant).to have_key(:type)
+        expect(merchant[:type]).to eq('merchant')
+
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a(String)
+      end
+    end
+
+    it 'shows page 1 is the same list of first 20' do
+      get '/api/v1/merchants'
+      first_20 = JSON.parse(response.body, symbolize_names: true)
+
+      get '/api/v1/merchants?page=1'
+      expect(response).to be_successful
+      page_1 = JSON.parse(response.body, symbolize_names: true)
+
+      expect(first_20[:data]).to eq(page_1[:data])
+    end
+
+    it 'shows second page of 20 merchants' do
+      get '/api/v1/merchants?page=1'
+      page_1 = JSON.parse(response.body, symbolize_names: true)
+
+      get '/api/v1/merchants?page=2'
+
+      expect(response).to be_successful
+
+      page_2 = JSON.parse(response.body, symbolize_names: true)
+
+      expect(page_2[:data].count).to eq(20)
+
+      page_1_first_id = page_1[:data].first[:id].to_i
+      page_2_first_id = page_1_first_id + 20
+
+      expect(page_2[:data].first[:id].to_i).to eq(page_2_first_id)
+    end
+
+    it 'shows first page of 50 merchants' do
+      get '/api/v1/merchants?per_page=50'
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants[:data].count).to eq(50)
+    end
+
+    it 'shows a blank page of merchants which would contain no data' do
+      get '/api/v1/merchants?page=200'
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants[:data].count).to eq(0)
+    end
+
+    it 'shows all merchants if per page is really big' do
+      get '/api/v1/merchants?per_page=200'
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants[:data].count).to eq(100)
+    end
+  end
+
+  describe 'sad path' do
+    it 'shows page 1 if page is 0 or lower' do
+      get '/api/v1/merchants?page=1'
+      page_1 = JSON.parse(response.body, symbolize_names: true)
+
+      get '/api/v1/merchants?page=0'
+      expect(response).to be_successful
+      page_0 = JSON.parse(response.body, symbolize_names: true)
+
+      expect(page_1[:data]).to eq(page_0[:data])
     end
   end
 end
