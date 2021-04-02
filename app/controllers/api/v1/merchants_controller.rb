d91e0e7de30a808b33ae9bfccfe4ff6c3806106b
abreaux26 <angelbreaux26@gmail.com>
2021-04-02 15:24:02 -0500

    Refactor get all merchants

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,25 +1,6 @@
 class Api::V1::MerchantsController < ApplicationController
-  # before_action :set_page, :set_per_page, only: [:index]
-
   def index
-    @page = params.fetch(:page,1).to_i
-    @per_page = params.fetch(:per_page,20).to_i
-
-    merchants = Merchant.limit(@per_page)
-
-    if @page > 1
-      merchants = merchants.offset((@page-1) * 20)
-    end
-
+    merchants = Merchant.pagination(per_page, page)
     render json: MerchantSerializer.format_merchants(merchants)
   end
-
-  # private
-  # def set_page
-  #   @page = params.fetch(:page,1).to_i
-  # end
-  #
-  # def set_per_page
-  #   @per_page = params.fetch(:per_page,20).to_i
-  # end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  include ActionController::Helpers
+
+  def page
+    params.fetch(:page, 1).to_i
+  end
+
+  def per_page
+    params.fetch(:per_page, 20).to_i
+  end
+
+  helper_method :page, :per_page
 end
