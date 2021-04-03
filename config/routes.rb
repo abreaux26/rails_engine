11c77296d81f3b09fb674020c00284b4ee76b912
abreaux26 <angelbreaux26@gmail.com>
2021-04-02 23:37:16 -0500

    Remove unwanted

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
       resources :merchants, path: "merchants(/:per_page/:page)" , only: [:index, :show] do
         resources :items, controller: 'merchant_items', only: :index
       end
-      # get '/merchants(/:per_page/:page)', to: 'merchants#index', as: 'merchants'
-      # get '/merchants/:id', to: 'merchants#show', as: 'merchant'
-      # get '/merchants/:id/items', to: 'merchant_items#index', as: 'merchant_items'
     end
   end
 end
