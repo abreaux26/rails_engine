00e0417c703e7872f25c6c15ff9da243d2576abc
abreaux26 <angelbreaux26@gmail.com>
2021-04-05 22:28:49 -0500

    Implement find all merchants

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :merchants, path: "merchants(/:per_page/:page)" , only: [:index, :show] do
         resources :items, controller: 'merchant_items', only: :index
+        collection do
+          get '/find_all(/:name)', to: 'merchants#find_all'
+        end
       end
       resources :items, path: "items(/:per_page/:page)" do
         resources :merchant, controller: 'items_merchant', only: :index
