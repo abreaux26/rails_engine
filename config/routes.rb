1cd3997d493c43df1ad18c983ef98b5b66073fcd
abreaux26 <angelbreaux26@gmail.com>
2021-04-06 20:40:29 -0500

    Remove unnecessary

--- config/routes.rb
+++ config/routes.rb
@@ -12,14 +12,10 @@ Rails.application.routes.draw do
 
       resources :merchants, only: [:index, :show] do
         resources :items, controller: :merchant_items, only: :index
-        # get :find_all, on: :collection, controller: :search
       end
-      # find_one_api_v1_items GET    /api/v1/items/find_one(.:format)  api/v1/search#find_one
 
       resources :items do
         resources :merchant, controller: :items_merchant, only: :index
-        # get :find_one, on: :collection, controller: :search
-        # resources :find_one, on: :collection, controller: :search
       end
     end
   end
