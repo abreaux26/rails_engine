676f4cbc28a5f86176a39b3095e648716c471bae
abreaux26 <angelbreaux26@gmail.com>
2021-04-08 22:14:09 -0500

    Change destroying an invoice again

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -13,4 +13,44 @@ RSpec.describe Invoice do
     it { should validate_presence_of :merchant_id }
     it { should validate_presence_of :status }
   end
+
+  before :each do
+    @item_1 = create(:item, name: 'Turing', unit_price: 45.00)
+    @item_2 = create(:item, name: 'Ring World', unit_price: 20.00)
+    @item_3 = create(:item, name: 'World', unit_price: 50.00)
+
+    @invoice = create(:invoice)
+    @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice, quantity: 2, unit_price: 5.00)
+  end
+
+  describe 'class methods' do
+    describe '::destroy_invoices' do
+      it 'has invoices to destory' do
+
+        expect(Invoice.destroy_invoices(@item_1.id)).to eq([@invoice])
+      end
+
+      it 'does not have invoices to destory' do
+        @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice)
+
+        expect(Invoice.destroy_invoices(@item_2.id)).to eq(nil)
+      end
+    end
+
+    describe '::other_items?' do
+      it 'has other items' do
+        @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice)
+
+        expect(Invoice.other_items?(@item_1.id)).to be_truthy
+      end
+
+      it 'does not have other items' do
+        expect(Invoice.other_items?(@item_1.id)).to be_falsy
+      end
+    end
+
+    it '::invoices_for' do
+      expect(Invoice.invoices_for(@item_1.id)).to eq([@invoice])
+    end
+  end
 end
